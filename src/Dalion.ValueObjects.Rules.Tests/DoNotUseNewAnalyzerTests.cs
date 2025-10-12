f6b52c7089902758203e6e103ba8aa52045f50ea
David Lievrouw <David.Lievrouw@coxautoinc.com>
2025-10-12 13:48:16 +0200

    Add first analyzer rule unit tests.

---
+++ src/Dalion.ValueObjects.Rules.Tests/DoNotUseNewAnalyzerTests.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace Dalion.ValueObjects.Rules;
+
+public class DoNotUseNewAnalyzerTests : RuleTests
+{
+    [Fact]
+    public async Task ProhibitsNewingUp()
+    {
+        await Run("var actual = [|new Dalion.ValueObjects.Samples.Celsius()|];");
+    }
+
+    [Fact]
+    public async Task AllowsCreationUsingFrom()
+    {
+        await Run("var actual = Dalion.ValueObjects.Samples.Celsius.From(24.4m);");
+    }
+}
---
+++ src/Dalion.ValueObjects.Rules.Tests/RuleTests.cs
@@ -0,0 +1,33 @@
+﻿using Dalion.ValueObjects.Samples;
+using Microsoft.CodeAnalysis.CSharp.Testing;
+using Microsoft.CodeAnalysis.Testing;
+using Xunit;
+
+namespace Dalion.ValueObjects.Rules;
+
+public abstract class RuleTests
+{
+    private static readonly string SamplesAssemblyLocation = typeof(Celsius).Assembly.Location;
+
+    protected async Task Run(string source, params DiagnosticResult[] expected)
+    {
+        var context = new CSharpAnalyzerTest<DoNotUseNewAnalyzer, DefaultVerifier>
+        {
+            ReferenceAssemblies = ReferenceAssemblies.Net.Net90.AddAssemblies(
+            [
+                "Dalion.ValueObjects",
+                SamplesAssemblyLocation.Replace(".dll", string.Empty),
+            ]),
+            TestCode = $@"
+public class Program {{
+    public void Main() {{
+        {source}
+    }}
+}}",
+        };
+
+        context.ExpectedDiagnostics.AddRange(expected);
+
+        await context.RunAsync(TestContext.Current.CancellationToken);
+    }
+}
